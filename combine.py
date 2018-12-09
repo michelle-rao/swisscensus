869ce91957e0494f0ff258978299bb2c7d6abb73
Michelle <m.rao3@lse.ac.uk>
2018-12-09 15:39:53 +0000

    code for merging files

--- combine.py
+++ combine.py
@@ -1,7 +1,52 @@
-#checking and combine files
-
+#This file checks for the files generated, and merges them together
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import os
+import glob
 #add checks for the files for a combined file
 
+datatype = ["_population", "_language_civilstatus", "_employment"]
+year = ["1980"]
+path ="C:/Users/Michelle/Desktop/Project/swisscensus/swisscensus/" # use your path
 
-#add tolerance level
-#return error name of the issue - take out district levels
+#Creating three separate files of datatype, "final_population", "final_language_civilstatus",
+#and "final_employment"
+
+frame = pd.DataFrame()
+list = []
+
+for type in datatype:
+    allFiles = glob.glob(path + type + "/*.csv" )
+        # for cant in canton:
+    for file in allFiles:
+        df = pd.read_csv(file,index_col=None, header=0)
+        list.append(df)
+    frame = pd.concat(list,sort = False)
+    frame.to_csv(type + "_final.csv", index=False)
+    list = []
+    frame = []
+
+#Would add checks here
+dflist = []
+
+#Cleaning datafiles
+for type in datatype:
+    df = pd.read_csv(type + "_final.csv")
+    df.rename({"Unnamed: 0":"a"}, axis = "columns", inplace = True)
+    df.drop(["a"],axis=1, inplace = True)
+    df.drop(['1','2','3','4','5','6','7','8','9','10','11','12'],axis=1, inplace=True)
+    df.set_index('mun_name', inplace = True)
+    dflist.append(df)
+
+left =dflist[0]
+middle = dflist[1]
+right = dflist[2]
+
+result1 = pd.merge(left, middle, on= 'mun_name', how="outer")
+result2 = pd.merge(result1, right, on= 'mun_name', how="outer")
+result2.drop(['13_x', '13_y'], axis=1, inplace=True)
+
+print(result2.head())
+result2.to_csv("_allvar_final.csv", index = True)
--- visual.py
+++ visual.py
@@ -1,19 +1,30 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
-filename = 'GL_1980_test.csv'
-df1 = pd.read_csv(filename)
+filename = '_allvar_final.csv'
+df= pd.read_csv(filename)
 sns.set()
 
-print(df1.head())
+print(df.head())
 #See if we can add these as questions in the command line
-xvar = "total_pop"
-yvar = "w_pop"
-cvar = "m_pop"
-graphtype = ['line', 'scatter', 'scatterfit', 'bar']
+
+df1= df[['mun_name', 'can_name','fem_pop','mun_pop','fem_employed']].copy()
+df1.dropna()
+print(df1.dtypes)
+
+df['fem_pop'] = df['fem_pop'].convert_objects(convert_numeric=True)
+df1['fem_pop']=df1.fem_pop.astype(float)
+# df1['fem_employed']=df1.fem_employed.astype(float)
+
+print(df1.dtypes)
+
+#df1['FLFP'] = df1['fem_pop']/ df1['mun_pop']
+
+
+xvar = "mun_name"
+yvar = "fem_employed"
 
 #page 26,27 (female labour force participation)
 #graphty= ['line', 'scatter', 'scatterfit', 'bar']
@@ -23,28 +34,28 @@ graphtype = ['line', 'scatter', 'scatterfit', 'bar']
 #Heatmap with 10 cantons; and two years with female labour force participation
 #Heatmap with cantons & percentage foreign population
 #Regression line graph with years and FLP
-
-#line graph
-sns.relplot(x= xvar, y= yvar, data= df1)
-plt.show()
-
-#scatter plot
-df1.plot.scatter(x=xvar,y=yvar,c= cvar ,cmap='coolwarm')
-plt.show()
-#y= data['married']/data['total_pop']
-
-#scatter plot with fitted line
-sns.lmplot(x=xvar, y=yvar, data=df1)
-plt.show()
-
-#barplot
-sns.barplot(x=xvar, y=yvar, data=df1)
-plt.show()
-
-#violinplot
-#sns.violinplot(x="day", y="total_bill", data=tips,hue='sex',palette='Set1')
-#plt.show()
-
-#matrixplots
-#flights.pivot_table(values='passengers',index='month',columns='year')
-#plt.show()
+#
+# #line graph
+# sns.relplot(x= xvar, y= yvar, data= df1)
+# plt.show()
+#
+# #scatter plot
+# df1.plot.scatter(x=xvar,y=yvar,c= cvar ,cmap='coolwarm')
+# plt.show()
+# #y= data['married']/data['total_pop']
+#
+# #scatter plot with fitted line
+# sns.lmplot(x=xvar, y=yvar, data=df1)
+# plt.show()
+#
+# #barplot
+# sns.barplot(x=xvar, y=yvar, data=df1)
+# plt.show()
+#
+# #violinplot
+# #sns.violinplot(x="day", y="total_bill", data=tips,hue='sex',palette='Set1')
+# #plt.show()
+#
+# #matrixplots
+# #flights.pivot_table(values='passengers',index='month',columns='year')
+# #plt.show()
